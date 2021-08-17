47e124c76180279dcc2f2963647c1bf523bcafcf
gabyypants <86713756+gabyypants@users.noreply.github.com>
2021-08-17 15:46:50 +0200

    API integration to enable retrieval of current weather features upon use of search engine/current location button

--- src/app.js
+++ src/app.js
@@ -38,3 +38,54 @@ let date = document.querySelector("#date");
 date.innerHTML = `${currentDay} ${currentDate}th ${currentMonth}`;
 let timestamp = document.querySelector("#timestamp");
 timestamp.innerHTML = `${hour}:${minutes}`;
+
+function currentWeather(response) {
+  // console.log(response.data.wind.direction);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#windspeed").innerHTML = `Wind: ${Math.round(
+    response.data.wind.speed
+  )} m/s `;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  // console.log(city);
+  // console.log(currentTemperature);
+  // console.log(currentDescription);
+  // console.log(currentWind);
+  // console.log(currentHumidity);
+}
+function search(city) {
+  let apiKey = "d4e31f25da5e889fefeac7617a05a07c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(currentWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  search(city);
+}
+
+let searchLocation = document.querySelector("#search");
+searchLocation.addEventListener("submit", handleSubmit);
+
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "d4e31f25da5e889fefeac7617a05a07c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(currentWeather);
+}
+function handleCurrentLocation() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", handleCurrentLocation);
+
+search("Stockholm");
